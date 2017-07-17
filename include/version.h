a955f0d58c3dca24e9de80698918edf0ce954cc9
Patrick Joseph Mcgee <pmcgee@mawdryn.mscs.mu.edu>
2017-07-17 17:08:15 -0500

    Fixed some compile / syntax errors / merge conflicts. Running.

--- include/version.h
+++ include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi3) #1 (pmcgee@mawdryn.mscs.mu.edu) Mon Jul 17 16:10:00 CDT 2017"
+#define VERSION "(Embedded Xinu) (arm-rpi3) #2 (pmcgee@mawdryn.mscs.mu.edu) Mon Jul 17 17:06:59 CDT 2017"
