311d8f802cfbe4c6fadffbbcd33738ed9717a305
Jl5her <Jl5her@yahoo.com>
2019-06-18 19:14:48 -0500

    Revert "WIP test multicore kprintf"
    This reverts commit 02093072785c1b76fee0074b97409d911a2610f7.

--- shell/xsh_test.c
+++ shell/xsh_test.c
@@ -29,108 +29,16 @@ extern bool screen_initialized;
  * @param args  array of arguments
  * @return non-zero value on error
  */
-
-int testmain(int argc, char **argv)
-{
-    uint cpuid = getcpuid();
-    int i = 0;
-    kprintf("Hello XINU World, from core %d!\r\n", cpuid);
-
-    for (i = 0; i < 10; i++)
-    {
-        kprintf("This is process %d\r\n", thrcurrent[cpuid]);
-
-        /* Uncomment the resched() line for cooperative scheduling. */
-       	// resched();
-    }
-    return 0;
-}
-
-void printpcb(int pid)
-{
-    struct thrent *ppcb = NULL;
-
-    /* Using the process ID, access it in the PCB table. */
-    ppcb = &thrtab[pid];
-
-    /* Printing PCB */
-    kprintf("Process name		  : %s \r\n", ppcb->name);
-
-    switch (ppcb->state)
-    {
-    case THRFREE:
-        kprintf("State of the process	  : FREE \r\n");
-        break;
-    case THRCURR:
-        kprintf("State of the process 	  : CURRENT \r\n");
-        break;
-    case THRSUSP:
-        kprintf("State of the process	  : SUSPENDED \r\n");
-        break;
-    case THRREADY:
-        kprintf("State of the process	  : READY \r\n");
-        break;
-    default:
-        kprintf("ERROR: Process state not correctly set!\r\n");
-        break;
-    }
-
-    /* Print PCB contents and registers */
-    kprintf("Base of run time stack    : 0x%08X \r\n", ppcb->stkbase);
-    kprintf("Stack length of process   : %8u \r\n", ppcb->stklen);
-}
-
 shellcmd xsh_test(int nargs, char *args[])
 {
-    int c, pid;
+	kprintf("\r\n\n*** SCREEN INITIALIZED?: %d\r\n\n", screen_initialized);
 
-    kprintf("0) Test creation of one process\r\n");
-    kprintf("1) Test passing of many args\r\n");
-    kprintf("2) Create three processes and run them\r\n");
-    kprintf("3) Create three processes and run them on other cores\r\n");
-
-    kprintf("===TEST BEGIN===\r\n");
-
-    // TODO: Test your operating system!
-
-    device *devptr;
-    devptr = (device *)&devtab[SERIAL0];
-    c = kgetc(devptr);
-    switch (c)
-    {
-    case '0':
-		// Process creation testcase
-        pid = create((void *)testmain, INITSTK, "MAIN1", 2, 0, NULL);
-        printpcb(pid);
-        break;
-
-    case '1':
-		// Many arguments testcase
-        //pid = create((void *)testbigargs, INITSTK, "MAIN1", 8,
-        //             0x11111111, 0x22222222, 0x33333333, 0x44444444,
-        //             0x55555555, 0x66666666, 0x77777777, 0x88888888);
-        printpcb(pid);
-        // TODO: print out stack with extra args
-        // TODO: ready(pid, RESCHED_YES, 0);
-        break;
-
-    case '2':
-        // Create three copies of a process, and let them play.
-        ready(create((void *)testmain, INITSTK, "MAIN1", 2, 0, NULL), RESCHED_NO , 0);
-        ready(create((void *)testmain, INITSTK, "MAIN2", 2, 0, NULL), RESCHED_NO , 0);
-        ready(create((void *)testmain, INITSTK, "MAIN3", 2, 0, NULL), RESCHED_YES, 0);
-        break;
-	
-	case '3':
-		// Create 3 processes and ready them on cores 1, 2, 3
-		ready(create((void *)testmain, INITSTK, "MAIN1", 2, 0, NULL), RESCHED_NO, 1);
-		ready(create((void *)testmain, INITSTK, "MAIN2", 2, 0, NULL), RESCHED_NO, 2);
-		ready(create((void *)testmain, INITSTK, "MAIN3", 2, 0, NULL), RESCHED_NO, 3);
-		break;
-	default:
-		break;
-    }
-
-    kprintf("\r\n===TEST END===\r\n");
-    return;
+	/* Test FPU (double precision): */
+	/*double a = 3.0;
+	double b = 1.5;
+	double result = a / b;
+	printf("%f / %f = %f\r\n", a, b, result);
+	*/
+	return OK;
 }
+
